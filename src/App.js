d5c442dd2ccf894a8a3d27b6786e7ecb011cee86
Phong <dovanphong96@gmail.com>
2019-06-12 00:23:21 +0700

    12/6/2019 dieu huong voi reacjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Headfile from './component/Headfile';
+import DieuHuong from './component/DieuHuong';
+import Footer from './component/Footer';
+import List from './component/List';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Headfile />
+        <DieuHuong />
+        <Footer />
+      </Router>
     </div>
   );
 }
